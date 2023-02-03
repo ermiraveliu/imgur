457a21300d861702b22b5602d9f0ed8472d07669
ermiraveliu <114769521+ermiraveliu@users.noreply.github.com>
2023-02-03 15:55:09 +0100

    Comments fix

--- src/components/GalleryPageComponents/Comments.tsx
+++ src/components/GalleryPageComponents/Comments.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import IComment from "../../interfaces/IComment";
 import Comment from "./Comment";
 interface CommentsProps {
@@ -8,9 +9,35 @@ interface CommentsProps {
 const Comments: React.FC<CommentsProps> = () => {
     const [comments, setComments] = useState<IComment[]>([] as IComment[])
     // const {post} = useContext(PostContext);
-    useEffect(()=> {
+     const endpoints = [
+      {
+      postId: "6rXPASw",
+      api: "https://api.npoint.io/ea0dfdf982eb0af3376d"
+      },
+      {
+         postId: "HnDIHVv",
+         api: "https://api.npoint.io/93795303798c52950840"
+      },
+      {
+         postId: "Ac7P2Jz",
+         api: "https://api.npoint.io/50d26d06a80889d6d9aa"
+      },
+   ]
+   let post = useParams();
+   let postId = post.id
+   let en:string = "" ;
+   endpoints.forEach(endpoint => { 
+      if(endpoint.postId === postId){
+         en = endpoint.api
+      }
+      }
+   )
+
+   useEffect(()=> {
        const getComments = async () => {
-        const res = await fetch("https://api.npoint.io/ea0dfdf982eb0af3376d");
+console.log(en, "fdghjkljvcgfhjioigcfvhjpigcb vhjiphvcg");
+        const res = await fetch(en);
+        
         const resJson = await res.json();
         console.log(resJson);
         setComments(resJson)
